ed9cc36184837e74b5e517cfdd8a82b178cf276a
omaier <oskar.maier@googlemail.com>
2012-05-28 16:47:46 +0200

    Changes module name from utilities.nibabel to utilities.nibalu to avoid confusion.

--- src/medpy/utilities/nibabelu.py
+++ src/medpy/utilities/nibabelu.py
@@ -5,6 +5,7 @@
 # build-in modules
 
 # third-party modules
+from nibabel.spatialimages import SpatialImage 
 
 # path changes
 
@@ -12,13 +13,25 @@
 
 # information
 __author__ = "Oskar Maier"
-__version__ = "d0.1, 2011-12-12"
+__version__ = "d0.2.0, 2011-12-12"
 __email__ = "oskar.maier@googlemail.com"
 __status__ = "Development" # tested functions marked with tested keyword
 __description__ = "NiBabel image utility functions."
 
 # code
-def image_like(data, reference, origin = None):
+def image(data):
+    """
+    Creates a NiBabel image for the supplied data. The image object will be of a generic
+    type and only made concrete during the saving process (nibabel.loadsave.save() takes
+    care of this).
+    @param data: a numpy array of arbitrary type
+    @return: a NiBabel image for data of generic type SpatialImage
+    """
+    image = SpatialImage(data, None)
+    image.update_header()
+    return image
+
+def image_like(data, reference):
     """
     Creates a NiBabel image for the data of the same type as image. Where the data
     is not sufficient to provide required information, the header attributes of image
@@ -34,7 +47,7 @@ def image_like(data, reference, origin = None):
     header = reference.get_header().copy()
     affine = reference.get_affine()
     # !TODO: This is not working correct: Somehow ItkSnap shows x/y origins to be negative. Why?
-    if None != origin: affine[:len(origin),-1] = origin
+    #if None != origin: affine[:len(origin),-1] = origin
     image = reference.__class__(data, affine, header=header)
     image.update_header()
     # !TODO: Is the following required? Works only for Nifty1. See the Nifty1 documentation/definition.
