ce0b3b2afa5358e2018cb4f1e21180849edb6696
omaier <oskar.maier@googlemail.com>
2012-08-24 12:00:02 +0200

    Daily backup. Crone scheduled.

--- src/medpy/application/miccai12/temporary.py
+++ src/medpy/application/miccai12/temporary.py
@@ -27,30 +27,31 @@ __description__ = """
 
 # code
 def main():
-    #degree = 100
-    shift = (0, 0, 00)
-    plane=(2, 1)
-    
     args = getArguments(getParser())
-    degree = args.angle
-
+    
     # prepare logger
     logger = Logger.getInstance()
     if args.debug: logger.setLevel(logging.DEBUG)
     elif args.verbose: logger.setLevel(logging.INFO)
     
-    # load input image
-    data_input, header_input = load(args.input)
-                                      
-    # rotate
-    data_input = ndimage.interpolation.rotate(data_input, degree, axes=plane, reshape=False)
-    data_input[data_input > 0] = 1
-    
-    # shift
-    data_input = ndimage.interpolation.shift(data_input, shift)
-    
-    # save image
-    save(data_input, args.output, header_input, args.force)
+    with open(args.output, 'w') as outf:        
+        outf.write('C;;E;;D;;ED;(std);ES;(std);Total;(std)')        
+        with open(args.input, 'r') as inf:            
+            for line in inf.readlines():                
+                line = line.strip()                
+                if 'ced' == line[0:3]:
+                    _tmp = line.split('_')[1:]
+                    _c = _tmp[0].split('-')
+                    _e = _tmp[1].split('-')
+                    _d = _tmp[2].split('-')
+                    outf.write('\n')
+                    outf.write('{};{};{};{};{};{};'.format(_c[0][1:], _c[1], _e[0][1:], _e[1], _d[0][1:], _d[1]))
+                elif 'Mean (std) endo DM' in line:
+                    _tmp = line.split(' ')
+                    outf.write('{};{};{};{};'.format(_tmp[5], _tmp[6], _tmp[8], _tmp[9]))
+                elif 'Total mean (std) endo DM' in line:
+                    _tmp = line.split(' ')
+                    outf.write('{};{}'.format(_tmp[5], _tmp[6]))
     
     logger.info("Successfully terminated.")    
     
@@ -63,7 +64,6 @@ def getParser():
     parser = argparse.ArgumentParser(description=__description__)
     parser.add_argument('input')
     parser.add_argument('output')
-    parser.add_argument('-a', dest='angle', type=int, help='Rotation angle.')
     parser.add_argument('-v', dest='verbose', action='store_true', help='Display more information.')
     parser.add_argument('-d', dest='debug', action='store_true', help='Display debug information.')
     parser.add_argument('-f', dest='force', action='store_true', help='Silently override existing output images.')
