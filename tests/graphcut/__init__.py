0976514602fbf29676f40447749aad9bbf879962
Oskar Maier <oskar.maier@googlemail.com>
2013-09-16 14:02:09 +0200

    Deletions connected with the previous commit.

--- tests/graphcut/__init__.py
+++
@@ -1,6 +0,0 @@
-# !TODO: Update these imports
-from cut import TestCut
-from graph import TestGraph
-from generate import TestGenerate
-from energy_label import TestEnergyLabel
-from energy_voxel import TestEnergyVoxel
