8d9feb458906f4c1970b0c8707ccb88804c2cf71
Oskar Maier <oskar.maier@googlemail.com>
2014-03-14 13:59:53 +0100

    Extended and released the medpy.metric.binary package.

--- medpy/metric/__init__.py
+++ medpy/metric/__init__.py
@@ -11,6 +11,7 @@ Modules:
     - volume: Holds a class to compute and extract volume similarities as used in (1).
     - histogram: Holds a number of real or near histogram distance metrics.
     - image: Metrics computed directly on the image intensities.
+    - binary: Metrics to compare binary objects in images.
 
 (1) The MICCAI 2997 Grand Challenge: Heimann T. et al. / "Comparison and Evaluation of
 Methods for Liver Segmentation From CT Datasets" / IEEE Transactions on Medical Imaging,
@@ -21,6 +22,7 @@ Vol.28, No.8, August 2009
 __all__ = []
 
 # if __all__ is not set, only the following, explicit import statements are executed
+from binary import dc, hd, asd, assd, precision, recall
 from surface import Surface
 from volume import Volume
 from histogram import *
--- medpy/metric/binary.py
+++ medpy/metric/binary.py
@@ -3,9 +3,9 @@
 Provides a number of binary image distance measures.
 
 @author Oskar Maier
-@version d0.1.0
+@version r0.1.0
 @since 2014-03-13
-@status Development
+@status Release
 """
 
 # build-in modules
@@ -13,24 +13,141 @@ Provides a number of binary image distance measures.
 # third-party modules
 import numpy
 from scipy.ndimage import _ni_support
-from scipy.ndimage.morphology import distance_transform_edt, binary_erosion
+from scipy.ndimage.morphology import distance_transform_edt, binary_erosion,\
+    generate_binary_structure
 
 # own modules
 
 # code
 def dc(input1, input2):
-    pass
+    """
+    Dice coefficient
+    
+    Computes the Dice coefficient (also known as Sorensen index) between the binary
+    objects in two images.
+    
+    The metric is defined as
+    
+    .. math::
+        
+        DC = \frac{2|A\capB|}{|A|+|B|}
+        
+    , where A is the first and B the second set of samples (here binary objects).
+    
+    Parameters
+    ----------
+    input1: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    input2: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    
+    Returns
+    -------
+    dc: float
+        The Dice coefficient between the object(s) in `input1` and the
+        object(s) in `input2`. It ranges from 0 (no overlap) to 1 (perfect overlap).
+        
+    Notes
+    -----
+    This is a real metric.
+    """
+    input1 = numpy.atleast_1d(input1.astype(numpy.bool))
+    input2 = numpy.atleast_1d(input2.astype(numpy.bool))
+    
+    intersection = numpy.count_nonzero(input1 & input2)
+    
+    size_i1 = numpy.count_nonzero(input1)
+    size_i2 = numpy.count_nonzero(input2)
+    
+    dc = 2. * intersection / float(size_i1 + size_i2)
+    
+    return dc
+
 
 def precision(input1, input2):
-    pass
+    """
+    Precison.
+    
+    Parameters
+    ----------
+    input1: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    input2: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    
+    Returns
+    -------
+    precision: float
+        The precision between two binary datasets, here mostly binary objects in images,
+        which is defined as the fraction of retrieved instances that are relevant. The
+        precision is not symmetric.
+    
+    See also
+    --------
+    :func:`recall`
+    
+    Note
+    ----
+    Not symmetric. The inverse of the precision is :func:`recall`.
+    High precision means that an algorithm returned substantially more relevant results than irrelevant.
+    """
+    input1 = numpy.atleast_1d(input1.astype(numpy.bool))
+    input2 = numpy.atleast_1d(input2.astype(numpy.bool))
+        
+    tp = numpy.count_nonzero(input1 & input2)
+    fp = numpy.count_nonzero(input1 & ~input2)
+    
+    precison = tp / float(tp + fp)
+    
+    return precison
 
 def recall(input1, input2):
-    pass
+    """
+    Recall
+    
+    Parameters
+    ----------
+    input1: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    input2: array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    
+    Returns
+    -------
+    recall: float
+        The recall between two binary datasets, here mostly binary objects in images,
+        which is defined as tthe fraction of relevant instances that are retrieved. The
+        recall is not symmetric.
+    
+    while recall (also known as sensitivity) is the fraction of relevant instances that are retrieved.
+    In simple terms, high precision means that an algorithm returned substantially more relevant results than irrelevant, while high recall means that an algorithm returned most of the relevant results.
+    
+    See also
+    --------
+    :func:`precision`
+    
+    Note
+    ----
+    Not symmetric. The inverse of the recall is :func:`precision`.
+    High recall means that an algorithm returned most of the relevant results.
+    """
+    input1 = numpy.atleast_1d(input1.astype(numpy.bool))
+    input2 = numpy.atleast_1d(input2.astype(numpy.bool))
+        
+    tp = numpy.count_nonzero(input1 & input2)
+    fn = numpy.count_nonzero(~input1 & input2)
 
-def fscore(input1, input2, beta=1):
-    pass
+    recall = tp / float(tp + fn)
+    
+    return recall
 
-def hd(input1, input2, voxelspacing=None):
+def hd(input1, input2, voxelspacing=None, connectivity=1):
     """
     Hausdorff Distance.
     
@@ -50,6 +167,11 @@ def hd(input1, input2, voxelspacing=None):
         along each dimension. If a sequence, must be of length equal to
         the input rank; if a single number, this is used for all axes. If
         not specified, a grid spacing of unity is implied.
+    connectivity: int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        scipy.ndimage.morphology.generate_binary_structure and should usually be > 1.
+        Presumably does not influence the result in the case of the Hausdorff distance.
         
     Returns
     -------
@@ -60,19 +182,19 @@ def hd(input1, input2, voxelspacing=None):
         
     See also
     --------
-    assd
-    asd
+    :func:`assd`
+    :func:`asd`
     
     Notes
     -----
     This is a real metric.
     """
-    hd1 = __surface_distances(input1, input2, voxelspacing).max()
-    hd2 = __surface_distances(input2, input1, voxelspacing).max()
+    hd1 = __surface_distances(input1, input2, voxelspacing, connectivity).max()
+    hd2 = __surface_distances(input2, input1, voxelspacing, ).max()
     hd = max(hd1, hd2)
     return hd
 
-def assd(input1, input2, voxelspacing=None):
+def assd(input1, input2, voxelspacing=None, connectivity=1):
     """
     Average symmetric surface distance.
     
@@ -92,6 +214,12 @@ def assd(input1, input2, voxelspacing=None):
         along each dimension. If a sequence, must be of length equal to
         the input rank; if a single number, this is used for all axes. If
         not specified, a grid spacing of unity is implied.
+    connectivity: int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        scipy.ndimage.morphology.generate_binary_structure and should usually be > 1.
+        The decision on the connectivity is important, as it can influence the results
+        strong. If in doubt, leave it as it is.         
         
     Returns
     -------
@@ -102,8 +230,8 @@ def assd(input1, input2, voxelspacing=None):
         
     See also
     --------
-    asd
-    hd
+    :func:`asd`
+    :func:`hd`
     
     Notes
     -----
@@ -112,10 +240,10 @@ def assd(input1, input2, voxelspacing=None):
     and
     >>>> asd(input2, input1)
     """
-    assd = ( asd(input1, input2, voxelspacing) + asd(input2, input1, voxelspacing) ) / 2.
+    assd = numpy.mean( (asd(input1, input2, voxelspacing, connectivity), asd(input2, input1, voxelspacing, connectivity)) )
     return assd
 
-def asd(input1, input2, voxelspacing=None):
+def asd(input1, input2, voxelspacing=None, connectivity=1):
     """
     Average surface distance metric.
     
@@ -134,6 +262,12 @@ def asd(input1, input2, voxelspacing=None):
         along each dimension. If a sequence, must be of length equal to
         the input rank; if a single number, this is used for all axes. If
         not specified, a grid spacing of unity is implied.
+    connectivity: int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        scipy.ndimage.morphology.generate_binary_structure and should usually be > 1.
+        The decision on the connectivity is important, as it can influence the results
+        strong. If in doubt, leave it as it is.        
     
     Returns
     -------
@@ -144,8 +278,8 @@ def asd(input1, input2, voxelspacing=None):
         
     See also
     --------
-    assd
-    hd
+    :func:`assd`
+    :func:`hd`
     
     
     Notes
@@ -157,12 +291,49 @@ def asd(input1, input2, voxelspacing=None):
     
     Examples
     --------
+    The `connectivity` determines what pixels/voxels are considered the surface of a
+    binary object. Take the following binary image showing a cross
+    >>> from scipy.ndimage.morphology import generate_binary_structure
+    >>> cross = generate_binary_structure(2, 1)
+    array([[0, 1, 0],
+           [1, 1, 1],
+           [0, 1, 0]])
+    With `connectivity` set to `1` a 4-neighbourhood is considered when determining the
+    object surface, resulting in
+    array([[0, 1, 0],
+           [1, 0, 1],
+           [0, 1, 0]])
+    Changing `connectivity` to `2`, a 8-neighbourhood is considered and we get:
+    array([[0, 1, 0],
+           [1, 1, 1],
+           [0, 1, 0]])
+    , as a diagonal connection does no longer qualifies as valid object surface.
+    
+    This influences the  results `asd` returns. Imagine we want to compute the surface
+    distance of our cross to a cube-like object:
+    >>>> cube = generate_binary_structure(2, 1)
+    array([[1, 1, 1],
+           [1, 1, 1],
+           [1, 1, 1]])
+    , which surface is, independent of the `connectivity` value set, always
+    array([[1, 1, 1],
+           [1, 0, 1],
+           [1, 1, 1]])
+           
+    Using a `connectivity` of `1` we get
+    >>>> asd(cross, cube, connectivity=1)
+    0.0
+    while a value of `2` returns uns
+    >>>> asd(cross, cube, connectivity=2)
+    0.20000000000000001
+    due to the center of the cross being considered surface as well.
+    
     """
-    sds = __surface_distances(input1, input2, voxelspacing)
+    sds = __surface_distances(input1, input2, voxelspacing, connectivity)
     asd = sds.mean()
     return asd
     
-def __surface_distances(input1, input2, voxelspacing=None):
+def __surface_distances(input1, input2, voxelspacing=None, connectivity=1):
     """
     The distances between the surface voxel of binary objects in input1 and their
     nearest partner surface voxel of a binary object in input2.
@@ -175,13 +346,17 @@ def __surface_distances(input1, input2, voxelspacing=None):
         if not voxelspacing.flags.contiguous:
             voxelspacing = voxelspacing.copy()
             
+    # binary structure
+    footprint = generate_binary_structure(input1.ndim, connectivity)
+            
     # extract only 1-pixel border line of objects
-    input1_border = input1 - binary_erosion(input1, iterations=1)
+    input1_border = input1 - binary_erosion(input1, structure=footprint, iterations=1)
+    input2_border = input2 - binary_erosion(input2, structure=footprint, iterations=1)
     
     # compute average surface distance        
     # Note: scipys distance transform is calculated only inside the borders of the
     #       foreground objects, therefore the input has to be reversed
-    dt = distance_transform_edt(~input2, sampling=voxelspacing)
+    dt = distance_transform_edt(~input2_border, sampling=voxelspacing)
     sds = dt[input1_border]
     
     return sds    
