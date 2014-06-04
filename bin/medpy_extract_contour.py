e6ad0f6ce788f579eea61ebde70e4d36d8ac4e0b
Oskar Maier <oskar.maier@googlemail.com>
2014-06-04 12:03:08 +0200

    Updated extract contour script to allow extraction of (N-1)D contours.

--- bin/medpy_extract_contour.py
+++ bin/medpy_extract_contour.py
@@ -26,7 +26,8 @@ import math
 
 # third-party modules
 import numpy
-from scipy.ndimage.morphology import binary_erosion, binary_dilation
+from scipy.ndimage.morphology import binary_erosion, binary_dilation,\
+    generate_binary_structure
 
 # path changes
 
@@ -45,6 +46,11 @@ __description__ = """
                   contour width, the surface of the volume will correspond with the
                   middle of the contour line. In the case of an odd contour width, the
                   contour will be shifted by one voxel towards the inside of the volume.
+                  
+                  In the case of 3D volumes, the contours result in shells, which might
+                  not be desired, as they do not visualize well in 2D views. With the
+                  '--dimension' argument, a dimension along which to extract the contours
+                  can be supplied.
                   
                   Copyright (C) 2013 Oskar Maier
                   This program comes with ABSOLUTELY NO WARRANTY; This is free software,
@@ -67,17 +73,32 @@ def main():
     # treat as binary
     data_input = data_input.astype(numpy.bool)
     
+    # check dimension argument
+    if args.dimension and (not args.dimension >= 0 or not args.dimension < data_input.ndim):
+        argparse.ArgumentError(args.dimension, 'Invalid dimension of {} supplied. Image has only {} dimensions.'.format(args.dimension, data_input.ndim))
+        
     # compute erosion and dilation steps
     erosions = int(math.ceil(args.width / 2.))
     dilations = int(math.floor(args.width / 2.))
     logger.debug("Performing {} erosions and {} dilations to achieve a contour of width {}.".format(erosions, dilations, args.width))
     
-    # erode and dilate
-    eroded = binary_erosion(data_input, iterations=erosions) if not 0 == erosions else data_input
-    dilated = binary_dilation(data_input, iterations=dilations) if not 0 == dilations else data_input
-    
-    # compute contour
-    data_output = dilated - eroded
+    # erode, dilate and compute contour
+    if not args.dimension:
+        eroded = binary_erosion(data_input, iterations=erosions) if not 0 == erosions else data_input
+        dilated = binary_dilation(data_input, iterations=dilations) if not 0 == dilations else data_input
+        data_output = dilated - eroded
+    else:
+        slicer = [slice(None)] * data_input.ndim
+        bs_slicer = [slice(None)] * data_input.ndim
+        data_output = numpy.zeros_like(data_input)
+        for sl in range(data_input.shape[args.dimension]):
+            slicer[args.dimension] = slice(sl, sl+1)
+            bs_slicer[args.dimension] = slice(1, 2)
+            bs = generate_binary_structure(data_input.ndim, 1)
+            
+            eroded = binary_erosion(data_input[slicer], structure=bs[bs_slicer], iterations=erosions) if not 0 == erosions else data_input[slicer]
+            dilated = binary_dilation(data_input[slicer], structure=bs[bs_slicer], iterations=dilations) if not 0 == dilations else data_input[slicer]
+            data_output[slicer] = dilated - eroded
     logger.debug("Contour image contains {} contour voxels.".format(numpy.count_nonzero(data_output)))
 
     # save resulting volume
@@ -98,6 +119,7 @@ def getParser():
     parser.add_argument('input', help='Source volume.')
     parser.add_argument('output', help='Target volume.')
     parser.add_argument('-w', '--width', dest='width', type=int, default=1, help='Width of the contour.')
+    parser.add_argument('--dimension', type=int, help='Extract contours only along this dimension.')
     parser.add_argument('-v', dest='verbose', action='store_true', help='Display more information.')
     parser.add_argument('-d', dest='debug', action='store_true', help='Display debug information.')
     parser.add_argument('-f', dest='force', action='store_true', help='Silently override existing output images.')
