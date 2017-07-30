cdc2bc1eef98e2d51bbd47ed2e99bb9c2c95f6ea
Pradeep Reddy Raamana <raamana@gmail.com>
2017-07-29 23:56:07 -0400

    - parameterizing/modularizing the tests, handling KL div exception - clear separation of options into metrics and otherwise

--- tests/metric_/histogram.py
+++ tests/metric_/histogram.py
@@ -10,15 +10,16 @@ from pytest import raises, warns, set_trace
 
 from medpy.metric import histogram
 
-metric_list_medpy_histogram = [
+metric_list = [
     'kullback_leibler', 'manhattan', 'minowski', 'euclidean',
-    'correlate', 'fidelity_based', 'quadratic_forms',
-    'cosine', 'cosine_1', 'cosine_2', 'cosine_alt',
+    'cosine_1',
     'noelle_2', 'noelle_4', 'noelle_5' ]
 
-unknown_medpy_histogram = ['histogram_intersection', ]
+unknown_prop_list = ['histogram_intersection']
+still_under_dev = ['quadratic_forms']
+similarity_func = ['correlate', 'cosine', 'cosine_2', 'cosine_alt', 'fidelity_based']
 
-semi_metric_list_medpy_histogram = [
+semi_metric_list = [
     'jensen_shannon', 'chi_square',
     'chebyshev', 'chebyshev_neg',
     'histogram_intersection_1',
@@ -26,41 +27,62 @@ semi_metric_list_medpy_histogram = [
     'noelle_1', 'noelle_3',
     'correlate_1']
 
-all_func_medpy_histogram = metric_list_medpy_histogram + semi_metric_list_medpy_histogram + unknown_medpy_histogram
+all_func_medpy_histogram = metric_list + semi_metric_list + similarity_func + unknown_prop_list + still_under_dev
 
-arbitrary_length = np.random.randint(1000)
-num_bins = 100
+arbitrary_length = np.random.randint(100, 1000)
+num_bins = np.random.randint(20, 200) # 100
+
+def within_tolerance(x, y):
+    "Function to indicate acceptable level of tolerance in numerical differences"
+
+    return np.allclose(x, y, rtol=1e-3, atol=np.finfo(float).eps)
 
 def make_random_histogram(length = arbitrary_length):
     "Returns a sequence of histogram density values that sum to 1.0"
 
     hist, bin_edges = np.histogram(np.random.random(arbitrary_length),
                                    bins = num_bins, density=True)
+
+    if len(hist) < 2:
+        raise ValueError('Invalid histogram')
+
     return hist
 
 def test_indiscernibility_metric():
 
     h1 = make_random_histogram()
 
-    for method_str in metric_list_medpy_histogram:
+    for method_str in metric_list:
         method = getattr(histogram, method_str)
-        assert np.allclose(method(h1, h1), 0.0, atol= np.finfo(float).eps)
+        is_indiscernible = within_tolerance(method(h1, h1), 0.0)
+        assert is_indiscernible
 
 def test_symmetry_metric():
 
     h1 = make_random_histogram()
     h2 = make_random_histogram()
 
-    for method_str in metric_list_medpy_histogram:
+    for method_str in metric_list:
         method = getattr(histogram, method_str)
-        assert np.allclose(method(h1, h2), method(h2, h1), atol= np.finfo(float).eps)
+        if method_str is 'kullback_leibler':
+            # for KL div : h1 can only contain zero values where h2 also contains zero values and vice-versa
+            h2[h1 == 0] = 0
+            h1[h2 == 0] = 0
+
+        d12 = method(h1, h2)
+        d21 = method(h2, h1)
+        if np.isnan(d12) or np.isnan(d21):
+            print method_str
+            print h1
+            print h2
+        assert within_tolerance(d12, d21)
 
 def test_nonnegativity_metric():
 
     h1 = make_random_histogram()
     h2 = make_random_histogram()
 
-    for method_str in metric_list_medpy_histogram:
+    for method_str in metric_list:
         method = getattr(histogram, method_str)
         assert method(h1,h2) >= 0.0
 
@@ -70,12 +92,20 @@ def test_triangle_inequality_metric():
     h2 = make_random_histogram()
     h3 = make_random_histogram()
 
-    for method_str in metric_list_medpy_histogram:
+    for method_str in metric_list:
         method = getattr(histogram, method_str)
+        if method_str is 'kullback_leibler':
+            # for KL div : h1 can only contain zero values where h2 also contains zero values and vice-versa
+            zeros_loc = np.logical_or(  h1 == 0, h2 == 0)
+            zeros_loc = np.logical_or(zeros_loc, h3 == 0)
+            h1[zeros_loc] = 0
+            h2[zeros_loc] = 0
+            h3[zeros_loc] = 0
+
         d12 = method(h1, h2)
         d23 = method(h2, h3)
         d13 = method(h1, h3)
         assert d12 <= d13 + d23
 
-
+test_symmetry_metric()
 test_indiscernibility_metric()
